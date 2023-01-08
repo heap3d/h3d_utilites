90161ad60d86962bd2c3a08448c093c8b40ada8d
heap3d <heap3d@gmail.com>
2023-01-08 03:14:41 +0200

    v1.14 h3d_debug.py: added: get_attributes added: get_name

--- scripts/h3d_debug.py
+++ scripts/h3d_debug.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # ================================
-# (C)2022 Dmytro Holub
+# (C)2022-2023 Dmytro Holub
 # heap3d@gmail.com
 # --------------------------------
 # modo python
@@ -11,23 +11,24 @@ import datetime
 import inspect
 import os.path
 import modo
+import h3d_utils as h3du
 
 
 class H3dDebug:
-    def __init__(self, enable=False, file=None, indent=0, indent_str='    '):
+    def __init__(self, enable=False, file=None, indent=0, indent_str=" " * 4):
         self.enable = enable
         self.initial_indent = int(indent)
         self.indent = self.initial_indent
         self.indent_str = indent_str
-        self.log_path = ''
+        self.log_path = ""
         self.file_init(file)
 
     def print_debug(self, message, indent=0):
         if not self.enable:
             return
         self.indent += indent
-        curtime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-        message_upd = '{}{} {}'.format(curtime, self.indent_str * self.indent, message)
+        curtime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        message_upd = "{}{} {}".format(curtime, self.indent_str * self.indent, message)
         if self.log_path:
             self.print_to_file(message_upd)
         else:
@@ -40,20 +41,33 @@ class H3dDebug:
         if not self.log_path:
             self.print_to_sys(message)
             return
-        with open(self.log_path, 'a') as f:
+        with open(self.log_path, "a") as f:
             f.write(message)
-            f.write('\n')
+            f.write("\n")
 
     def print_to_sys(self, message):
         if not self.enable:
             return
         print(message)
 
-    def exit(self, message='debug exit'):
+    def exit(self, message="debug exit"):
         if not self.enable:
             return
         self.print_debug(message)
         exit()
+
+    def get_name(self, item):
+        if not self.enable:
+            return
+        if not item:
+            return
+
+        try:
+            name = item.name
+        except AttributeError:
+            name = ""
+
+        return name
 
     def print_items(self, items, message=None, indent=0):
         if not self.enable:
@@ -63,35 +77,37 @@ class H3dDebug:
         if not items:
             self.print_debug(items, indent=indent + 1)
             return
-        
+
         for i in items:
-            if 'modo.item.' in str(type(i)):
-                self.print_debug('<{}> : <{}>'.format(i.name, i.type), indent=indent + 1)
+            if "modo.item." in str(type(i)):
+                self.print_debug(
+                    "<{}> : <{}>".format(i.name, h3du.safe_type(i)), indent=indent + 1
+                )
             else:
-                self.print_debug('<{}>'.format(i), indent=indent + 1)
+                self.print_debug("<{}>".format(i), indent=indent + 1)
 
     def file_init(self, file):
         if not file:
             return
-        
+
         scene_path = modo.Scene().filename
         if not scene_path:
             self.log_path = None
             return
-        
+
         scene_dir = os.path.dirname(scene_path)
         log_path = os.path.join(scene_dir, file)
         self.log_path = log_path
         if not self.enable:
             return
-        
+
         self.log_reset()
 
     def print_fn_in(self):
         if not self.enable:
             return
         caller = inspect.stack()[1][3]
-        message = '{}(): in >>>>'.format(caller)
+        message = "{}(): in >>>>".format(caller)
         self.print_debug(message)
         self.indent_inc()
 
@@ -100,7 +116,7 @@ class H3dDebug:
             return
         caller = inspect.stack()[1][3]
         self.indent_dec()
-        message = '<<<< out: {}()'.format(caller)
+        message = "<<<< out: {}()".format(caller)
         self.print_debug(message)
 
     def indent_inc(self, inc=1):
@@ -116,7 +132,22 @@ class H3dDebug:
     def log_reset(self):
         if not self.log_path:
             return
-        with open(self.log_path, 'w'):
+        with open(self.log_path, "w"):
             # reinitialize log file
             pass
         self.indent = self.initial_indent
+
+    def get_attributes(self, class_item):
+        members = inspect.getmembers(class_item)
+        public_members = []
+        for member in members:
+            # skip if starts with underscore
+            if member[0].startswith("_"):
+                continue
+            # skip if member is method
+            if inspect.ismethod(member[1]):
+                continue
+            # add to public members list
+            public_members.append(member)
+
+        return public_members
--- scripts/h3d_utils.py
+++ scripts/h3d_utils.py
@@ -12,114 +12,13 @@ import modo
 import modo.mathutils as mmu
 
 
-# obsolete class. leaved for compatibility
-class H3dUtils:
-    @staticmethod
-    def get_user_value(name):
-        value = lx.eval('user.value {} ?'.format(name))
-        return value
-
-    @staticmethod
-    def set_user_value(name, value):
-        lx.eval('user.value {} {{{}}}'.format(name, value))
-
-    @staticmethod
-    def parent_items_to(items, parent):
-        # clear selection
-        modo.Scene().deselect()
-        # select items
-        for item in items:
-            item.select()
-        # select parent item
-        parent.select()
-        # parent items to parent item
-        lx.eval('item.parent inPlace:1')
-
-    @staticmethod
-    def set_mesh_debug_info(mesh, info_str, debug_mode=False):
-        if not mesh:
-            return
-        if debug_mode:
-            mesh.select(replace=True)
-            lx.eval('item.tagAdd DESC')
-            lx.eval('item.tag string DESC "{}"'.format(info_str))
-
-    @staticmethod
-    def get_mesh_debug_info(mesh):
-        if not mesh:
-            return None
-
-        mesh.select(replace=True)
-        return lx.eval('item.tag string DESC ?')
-
-    @staticmethod
-    def get_full_mesh_area(mesh):
-        if not mesh:
-            return None
-        if mesh.type != 'mesh':
-            return 0.0
-
-        full_area = sum([poly.area for poly in mesh.geometry.polygons])
-        return full_area
-
-    @staticmethod
-    def merge_two_meshes(mesh1, mesh2):
-        if not mesh1:
-            return
-        if not mesh2:
-            return
-        lx.eval('select.type item')
-        mesh1.select(replace=True)
-        mesh2.select()
-        lx.eval('layer.mergeMeshes true')
-
-    @staticmethod
-    def get_mesh_bounding_box_size(mesh):
-        if not mesh:
-            return mmu.Vector3()
-        if not mesh.geometry.polygons:
-            return mmu.Vector3()
-        v1, v2 = map(mmu.Vector3, mesh.geometry.boundingBox)
-        return v2 - v1
-
-    @staticmethod
-    def get_source_of_instance(item):
-        if item is None:
-            return None
-        if not item.isAnInstance:
-            return item
-
-        try:
-            item_source = item.itemGraph('source').forward(0)
-        except LookupError:
-            print('No source of instance item found for <{}>'.format(item.name))
-            return None
-        if item_source.isAnInstance:
-            return H3dUtils.get_source_of_instance(item_source)
-        else:
-            return item_source
-
-    @staticmethod
-    def replace_file_ext(name='log', ext='.txt'):
-        basename = name.rsplit('.', 1)[0]
-        return '{}{}'.format(basename, ext)
-
-    @staticmethod
-    def type_int_to_str(type_int):
-        return lx.service.Scene().ItemTypeName(type_int)
-
-    @staticmethod
-    def type_str_to_int(type_str):
-        return lx.service.Scene().ItemTypeLookup(type_str)
-
-
 def get_user_value(name):
-    value = lx.eval('user.value {} ?'.format(name))
+    value = lx.eval("user.value {} ?".format(name))
     return value
 
 
 def set_user_value(name, value):
-    lx.eval('user.value {} {{{}}}'.format(name, value))
+    lx.eval("user.value {} {{{}}}".format(name, value))
 
 
 def parent_items_to(items, parent):
@@ -131,7 +30,7 @@ def parent_items_to(items, parent):
     # select parent item
     parent.select()
     # parent items to parent item
-    lx.eval('item.parent inPlace:1')
+    lx.eval("item.parent inPlace:1")
 
 
 def set_mesh_debug_info(mesh, info_str, debug_mode=False):
@@ -140,7 +39,7 @@ def set_mesh_debug_info(mesh, info_str, debug_mode=False):
         return
     if debug_mode:
         mesh.select(replace=True)
-        lx.eval('item.tagAdd DESC')
+        lx.eval("item.tagAdd DESC")
         lx.eval('item.tag string DESC "{}"'.format(info_str))
 
 
@@ -150,13 +49,13 @@ def get_mesh_debug_info(mesh):
         return None
 
     mesh.select(replace=True)
-    return lx.eval('item.tag string DESC ?')
+    return lx.eval("item.tag string DESC ?")
 
 
 def get_full_mesh_area(mesh):
     if not mesh:
         return None
-    if mesh.type != 'mesh':
+    if mesh.type != "mesh":
         return 0.0
 
     full_area = sum([poly.area for poly in mesh.geometry.polygons])
@@ -168,10 +67,11 @@ def merge_two_meshes(mesh1, mesh2):
         return
     if not mesh2:
         return
-    lx.eval('select.type item')
+
+    lx.eval("select.type item")
     mesh1.select(replace=True)
     mesh2.select()
-    lx.eval('layer.mergeMeshes true')
+    lx.eval("layer.mergeMeshes true")
 
 
 def get_mesh_bounding_box_size(mesh):
@@ -179,6 +79,7 @@ def get_mesh_bounding_box_size(mesh):
         return mmu.Vector3()
     if not mesh.geometry.polygons:
         return mmu.Vector3()
+
     v1, v2 = map(mmu.Vector3, mesh.geometry.boundingBox)
     return v2 - v1
 
@@ -190,24 +91,27 @@ def get_source_of_instance(item):
         return item
 
     try:
-        item_source = item.itemGraph('source').forward(0)
+        item_source = item.itemGraph("source").forward(0)
     except LookupError:
-        print('No source of instance item found for <{}>'.format(item.name))
+        print("No source of instance item found for <{}>".format(item.name))
         return None
     if item_source.isAnInstance:
-        return H3dUtils.get_source_of_instance(item_source)
-    else:
-        return item_source
+        return get_source_of_instance(item_source)
+
+    return item_source
 
 
-def replace_file_ext(name='log', ext='.txt'):
-    basename = name.rsplit('.', 1)[0]
-    return '{}{}'.format(basename, ext)
+def replace_file_ext(name="log", ext=".txt"):
+    basename = name.rsplit(".", 1)[0]
+    return "{}{}".format(basename, ext)
 
 
 def itype_str(type_int):
     """convert int modo item type to str type.
-    example: c.MESH_TYPE to 'mesh' """
+    example: c.MESH_TYPE to 'mesh'"""
+    if isinstance(type_int, str):
+        return type_int
+
     return lx.service.Scene().ItemTypeName(type_int)
 
 
@@ -219,17 +123,27 @@ def itype_int(type_str):
 
 def item_rotate(item, rads):
     if not item:
-        print('item_rotate(): not item.')
+        print("item_rotate(): not item.")
         return
     if not rads:
-        print('item_rotate(): not rads.')
+        print("item_rotate(): not rads.")
         return
     if len(rads) != 3:
-        print('item_rotate(): len(rads) != 3')
+        print("item_rotate(): len(rads) != 3")
         return
+
     # get current rotation
     rotation = mmu.Vector3(item.rotation.get())
     # update item rotation
     rotation_upd = (rotation.x + rads.x, rotation.y + rads.y, rotation.z + rads.z)
     # set item rotation
     item.rotation.set(rotation_upd)
+
+
+def safe_type(item):
+    if item not in modo.Scene().groups:
+        return item.type
+    if item.type == 'assembly':
+        return item.type
+    if item.type == '':
+        return 'group'
